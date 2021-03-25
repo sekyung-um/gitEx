660c83e4b7f579f475ed59ac98eaa9bdb58dba62
sekyung <69137900+sekyung-um@users.noreply.github.com>
2021-03-25 23:17:59 +0900

    Update script.js

--- 200325-2/js/script.js
+++ 200325-2/js/script.js
@@ -8,7 +8,18 @@ $(document).ready(function () {
   });
 
   //슬라이드
-
+    $(".slide li").eq(0).show();
+    $(".slide li").eq(0).siblings().hide();
+    
+    var i = 0;
+    setInterval(function(){
+    i++;
+    if(i==3) {
+        i=0;
+    } 
+    $(".slide li").eq(i).fadeIn(1000);
+    $(".slide li").eq(i-1).fadeOut(1000);
+    },3000);
   //모달창
   $(".notice li")
     .eq(0)
